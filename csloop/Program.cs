05cce21996fb46e59b541ac01b9fdaefba872107
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-08 12:25:18 +0900

    Thread.Sleep() 메서드 예제

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csloop
@@ -29,6 +30,23 @@ namespace csloop
             foreach (var item in array)
             {
                 Console.WriteLine(item);
+            }
+
+            int x = 1;
+            while(x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+                Thread.Sleep(100);
+                x++;
             }
         }
     }
